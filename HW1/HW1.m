e3bc6f492389adbe9da23ee57570bdf0f601a2ec
Kensuke Suzuki <kxs974@psu.edu>
2018-08-31 11:48:31 -0400

    Problem1

--- HW1/HW1.m
+++ HW1/HW1.m
@@ -5,4 +5,14 @@
 clear all
 %% Problem 1
 
-a = 1
+X = [1,1.5,3,4,5,7,9,10];
+Y1 = -2 + .5*X;
+Y2 = -2 + 0.5 * X.^2;
+
+plot(X, Y1, X, Y2)
+title('Problem 1: plot Y1 and Y2 against X')
+xlabel('X') % x-axis label
+ylabel('Y1 and Y2') % y-axis label
+legend('Y1 = -2 + .5X','Y2 = -2 + .5X^2')
+
+%%
